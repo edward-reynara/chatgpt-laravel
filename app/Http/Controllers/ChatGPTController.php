47333e9292286addf5429b653918645d014656c8
edward-reynara <edwardreynara@gmail.com>
2023-03-31 10:48:08 +0700

    update notes

--- app/Http/Controllers/ChatGPTController.php
+++ app/Http/Controllers/ChatGPTController.php
@@ -205,26 +205,8 @@ class ChatGPTController extends Controller
             ],
         ]);
         
-        // $response->id; // 'chatcmpl-6pMyfj1HF4QXnfvjtfzvufZSQq6Eq'
-        // $response->object; // 'chat.completion'
-        // $response->created; // 1677701073
-        // $response->model; // 'gpt-3.5-turbo-0301'
-        
-        // foreach ($response->choices as $result) {
-        //     $result->index; // 0
-        //     $result->message->role; // 'assistant'
-        //     $result->message->content; // '\n\nHello there! How can I assist you today?'
-        //     $result->finishReason; // 'stop'
-        // }
-        
-        // $response->usage->promptTokens; // 9,
-        // $response->usage->completionTokens; // 12,
-        // $response->usage->totalTokens; // 21
-        
-        // $response->toArray(); // ['id' => 'chatcmpl-6pMyfj1HF4QXnfvjtfzvufZSQq6Eq', ...]
-
         $result = $response->toArray();
-        $result = $response['choices'][0]['message']['content'];
+        // $result = $response['choices'][0]['message']['content'];
         
         // $result = $response['choices'][0]['text'];
         // $result = $response->choices[0]->text;
